591560b39f806a474d5272bef0bad2d2e88936a8
Luka Brdar <luka.brdar@tuta.io>
2026-04-15 23:57:23 +0200

    Coded windowing system. Nothing implamented thus

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,1 +1,16 @@
-// TODO: react-rnd wrapper
+import { Rnd } from 'react-rnd';
+
+export const Window = ({ title, children, defaultPos }) => {
+    <Rnd 
+    default={{ x: defaultPos.x, y: defaultPos.y, width: 500, height: 300 }}
+    bounds="parent"
+    style={{ border: '1px solid #555', background: '#222', borderRadius: '8px', overflow: 'hidden' }}
+    >
+        <div  style={{ background: '#444', padding: '4px', cursor: 'move', color: 'white', fontSize: '12px' }}>
+            {title} 
+        </div>   
+        <div style={{ height: 'calc(100%- 25px)' }}>
+            {children}
+        </div>
+    </Rnd>
+}
